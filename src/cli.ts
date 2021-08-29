65155da0164d1b2eb5da29002e326f84f980e608
Ryan Wessel <ryan@wesselstudios.com>
2021-08-29 16:58:58 -0400

    Recursively add contributors to config w/ interactive prompt

--- src/cli.ts
+++ src/cli.ts
@@ -1,46 +1,31 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import chalk from 'chalk';
-import fs from 'fs';
 
-const program = new Command();
+export const pear = new Command();
 
-program
-    .command('init')
-    .description('Initialize Pear in your project')
-    .action(() => {
-        console.info(chalk.yellow('🍐 Thanks for adding Pear to your project!'));
-        if (!fs.existsSync('./.pear')) {
-            console.info(chalk.yellow('.pear directory not found, creating it now.'));
-            fs.mkdirSync('./.pear');
-        }
-        if (!fs.existsSync('./.pear/contributors.json')) {
-            console.info(chalk.yellow('contributors.json not found, creating it now.'));
-            fs.writeFileSync('./.pear/contributors.json', '{}');
-        }
-    })
+import './commands/init';
 
-program
+pear
     .command('start')
     .description('Start a pair programming session')
 
-program
+pear
     .command('end')
     .description('End a pair programming session')
 
-program
+pear
     .command('add')
     .description('Remove a contributor from your Pear configuration')
     .argument('<contributor>', 'The name of the contributor you are adding')
 
-program
+pear
     .command('remove')
     .description('Remove a contributor from your Pear configuration')
     .argument('<contributor>', 'The name of the contributor you are removing')
 
-program
+pear
     .command('matrix')
     .description('Generate a pairing matrix based on your project\'s commit history')
 
-program.parse(process.argv);
+pear.parse(process.argv);
---
+++ src/commands/init.ts
@@ -0,0 +1,51 @@
+import chalk from "chalk";
+import { existsSync, mkdirSync, writeFileSync } from "fs";
+import { prompt } from "inquirer";
+import { pear } from "../cli";
+
+const createContributorsFile = (contributors: string[]) => {
+    console.info(chalk.yellow("Creating contributors file now"));
+    writeFileSync("./.pear/contributors", contributors.join("\n"));
+};
+
+const addContributor = (contributors: string[]) => {
+    const questions = [
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the name of a project contributor",
+      },
+      {
+        type: "confirm",
+        name: "moreToAdd",
+        message: "Would you like to add more contributors?",
+      },
+    ];
+
+    prompt(questions).then(({ name, moreToAdd }) => {
+      contributors.push(name);
+      if (moreToAdd) {
+        addContributor(contributors);
+      } else {
+        createContributorsFile(contributors);
+      }
+    });
+  };
+
+pear
+  .command("init")
+  .description("Initialize Pear in your project")
+  .action(() => {
+    console.info(chalk.yellow("🍐 Thanks for adding Pear to your project!"));
+
+    if (!existsSync("./.pear")) {
+      console.info(chalk.yellow(".pear directory not found, creating it now."));
+      mkdirSync("./.pear");
+    }
+    console.info(
+      chalk.yellow("Let's add contributors to your Pear configuration.")
+    );
+
+    const contributors: string[] = [];
+    addContributor(contributors);
+  });
