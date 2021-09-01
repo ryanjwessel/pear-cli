4bc02efe02b72ebc5908ac67e10f5f0ec42edc5f
Ryan Wessel <ryan@wesselstudios.com>
2021-09-01 17:31:40 -0400

    Append coauthors on shorthand commits
    Co-authors: Chuck Norris, Anderson Mesquita

--- .pear/pear.sh
+++ .pear/pear.sh
@@ -6,21 +6,14 @@ SHA1=$3
 
 filename='./.pear/session'
 
-echo $COMMIT_SOURCE
-echo $SHA1
-
 if [ ! -f "$filename" ]; then
+    # If the Pear session file does not exist, then there is no session.
     exit 0;
 fi
 
 if [ ! -z "$SHA1"]; then
     # If SHA1 exists, we are amending a commit. Don't modify the message.
     exit 0;
-fi
-
-if [[ "$COMMIT_SOURCE" == "message" ]]; then
-    echo $COMMIT_MSG_FILE
-    echo "short-hand commit"
 fi
 
 coauthors="Co-authors:"
@@ -31,4 +24,10 @@ while read line; do
 done < $filename
 coauthors="$(echo $coauthors | sed 's/,$//g')" # Remove trailing comma
 
-echo "\n\n${coauthors}\n$(cat $COMMIT_MSG_FILE)" > "$COMMIT_MSG_FILE"
+if [[ "$COMMIT_SOURCE" == "message" ]]; then
+    # Append coauthors if user has provided a shorthand commit message
+    echo "$(cat $COMMIT_MSG_FILE)\n\n${coauthors}\n" > "$COMMIT_MSG_FILE"
+else
+    # Prepend coauthors if user is using the 
+    echo "\n\n${coauthors}\n$(cat $COMMIT_MSG_FILE)" > "$COMMIT_MSG_FILE"
+fi
