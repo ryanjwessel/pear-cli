c3f6cb73425d1ab3ada72440324b55dc76b619ad
Ryan Wessel <ryan@wesselstudios.com>
2021-09-01 09:06:11 -0400

    Make end command use non-Promise rm command for better Node support

--- src/commands/end.ts
+++ src/commands/end.ts
@@ -1,15 +1,17 @@
 import chalk from "chalk";
-import { promises } from "fs";
+import { rm } from "fs";
 import { pear } from "../cli";
 
 pear
   .command("end")
   .description("End a pair programming session")
-  .action(async () => {
-    try {
-      await promises.rm("./.pear/session", { force: true });
+  .action(() => {
+    rm("./.pear/session", (err) => {
+      if (err) {
+        console.error(err);
+        console.error(chalk.redBright("Could not remove Pear's session file."));
+        return;
+      }
       console.info(chalk.yellowBright("Successfully ended pairing session."));
-    } catch (error) {
-      console.error(chalk.redBright("Could not remove Pear's session file."));
-    }
+    });
   });
