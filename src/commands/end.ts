ac1885f6c2ee7576553d52c23a97a2d3a3a8f1d0
Ryan Wessel <ryan@wesselstudios.com>
2021-09-07 10:00:06 -0400

    Use fs.unlink for wider Node file removal support

--- src/commands/end.ts
+++ src/commands/end.ts
@@ -2,7 +2,7 @@ import chalk from "chalk";
 import fs from "fs";
 
 export const end = () => {
-  fs.rm("./.pear/session", (err) => {
+  fs.unlink("./.pear/session", (err) => {
     if (err) {
       console.error(err);
       console.error(chalk.redBright("Could not remove Pear's session file."));
